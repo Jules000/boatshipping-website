816663c60cde64c1266432f0ad42a8574381eb7c
Jules000 <julesrenaud10@gmail.com>
2020-09-29 06:32:29 +0100

    disabling the right click

--- js/index.js
+++ js/index.js
@@ -1,3 +1,19 @@
+/*
+***********DISABLING THE RIGHT CLICK***************
+*/
+<!-- http://www.spacegun.co.uk -->
+
+var message = "function disabled";
+
+function rtclickcheck(keyp){ if (navigator.appName == "Netscape" && keyp.which == 3){ alert(message); return false; }
+
+if (navigator.appVersion.indexOf("MSIE") != -1 && event.button == 2) { alert(message); return false; } }
+
+document.onmousedown = rtclickcheck;
+
+
+
+
 const $body = $("body");
 const $header = $(".page-header");
 const scrollClass = "scroll";
